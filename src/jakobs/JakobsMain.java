8625bd41448139ebca4fe48a575f77aea05bb065
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2025-03-03 20:44:09 +0100

    added Fu, Blaz, Shapes0, Shapes1, Jakobs1, Jakobs2

---
+++ src/jakobs/JakobsMain.java
@@ -0,0 +1,35 @@
+package jakobs;
+
+import general.Util;
+import json.Instance;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipOutputStream;
+
+public class JakobsMain {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+        File parentFolder = new File(args[0]);
+        File outputFolder = new File(parentFolder.getAbsolutePath() + "_converted");
+        outputFolder.mkdir();
+
+        File inputFile = new File(parentFolder.getAbsolutePath() + "/jakobs2.xml");
+        Instance instance = JakobsParser.parseInstance(inputFile);
+
+        Util.writeInstance(instance, outputFolder);
+
+        //create zip archive
+        FileOutputStream fos = new FileOutputStream(outputFolder.getAbsolutePath() + ".zip");
+        ZipOutputStream zipOS = new ZipOutputStream(fos);
+
+        for (File file : outputFolder.listFiles()) {
+            Util.zipFile(file, file.getName(), zipOS);
+        }
+
+        zipOS.close();
+        fos.close();
+    }
+}
